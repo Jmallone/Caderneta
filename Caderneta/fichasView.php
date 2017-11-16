c56d66a5556f6c3c4235c75474dcecdaa529cd48
Michel G. Souza <Michelgomesdes@hotmail.com>
2017-11-15 23:38:23 -0200

    Add files via upload

---
+++ Caderneta/fichasView.php
@@ -0,0 +1,206 @@
+
+<div id="page-wrapper">
+
+  <div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="row">
+      <div class="col-lg-12">
+
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3>
+              <div class="row">
+                <div class="col-xs-6 col-sm-3">Zuran</div>
+
+                <div class="col-md-1">Druid</div>
+                <div class="col-xs-6 col-sm-3">Level: 6</div>
+              </div>
+              <div class="row">
+                <div class="col-xs-6 col-sm-3">Danilo</div>
+                <div class="col-md-1">Tiefling</div>
+                <div class="col-xs-6 col-sm-3">XP: 2600</div>
+              </div>
+            </h3>
+          </div>
+
+          <div class="panel-body">
+            <center>
+
+
+
+              <div class="row">
+                <div>
+
+
+
+
+                                <div class="col-lg-6">
+                                  <div class="row">
+                                    <div class="col-lg-3 col-md-6">
+                                      <div class="panel panel-default">
+                                        <div class="panel-header">
+                                          <span class="col-xs-9 text-right">STR</span>
+                                          <div class="clearfix"></div>
+                                        </div>
+                                        <div class="panel-heading">
+                                          <div class="row">
+
+                                            <div class="col-xs-9 text-right">
+                                              <div class="huge">12</div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <a href="#">
+                                          <div class="panel-footer">
+                                            <span class="col-xs-9 text-right">5</span>
+                                            <div class="clearfix"></div>
+                                          </div>
+                                        </a>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <div class="row">
+                                    <div class="col-lg-3 col-md-6">
+                                      <div class="panel panel-default">
+                                        <div class="panel-header">
+                                          <span class="col-xs-9 text-right">DEX</span>
+                                          <div class="clearfix"></div>
+                                        </div>
+                                        <div class="panel-heading">
+                                          <div class="row">
+
+                                            <div class="col-xs-9 text-right">
+                                              <div class="huge">12</div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <a href="#">
+                                          <div class="panel-footer">
+                                            <span class="col-xs-9 text-right">5</span>
+                                            <div class="clearfix"></div>
+                                          </div>
+                                        </a>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <div class="row">
+                                    <div class="col-lg-3 col-md-6">
+                                      <div class="panel panel-default">
+                                        <div class="panel-header">
+                                          <span class="col-xs-9 text-right">CON</span>
+                                          <div class="clearfix"></div>
+                                        </div>
+                                        <div class="panel-heading">
+                                          <div class="row">
+
+                                            <div class="col-xs-9 text-right">
+                                              <div class="huge">12</div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <a href="#">
+                                          <div class="panel-footer">
+                                            <span class="col-xs-9 text-right">5</span>
+                                            <div class="clearfix"></div>
+                                          </div>
+                                        </a>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <div class="row">
+                                    <div class="col-lg-3 col-md-6">
+                                      <div class="panel panel-default">
+                                        <div class="panel-header">
+                                          <span class="col-xs-9 text-right">WIS</span>
+                                          <div class="clearfix"></div>
+                                        </div>
+                                        <div class="panel-heading">
+                                          <div class="row">
+
+                                            <div class="col-xs-9 text-right">
+                                              <div class="huge">12</div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <a href="#">
+                                          <div class="panel-footer">
+                                            <span class="col-xs-9 text-right">5</span>
+                                            <div class="clearfix"></div>
+                                          </div>
+                                        </a>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <div class="row">
+                                    <div class="col-lg-3 col-md-6">
+                                      <div class="panel panel-default">
+                                        <div class="panel-header">
+                                          <span class="col-xs-9 text-right">INT</span>
+                                          <div class="clearfix"></div>
+                                        </div>
+                                        <div class="panel-heading">
+                                          <div class="row">
+
+                                            <div class="col-xs-9 text-right">
+                                              <div class="huge">12</div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <a href="#">
+                                          <div class="panel-footer">
+                                            <span class="col-xs-9 text-right">5</span>
+                                            <div class="clearfix"></div>
+                                          </div>
+                                        </a>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <div class="row">
+                                    <div class="col-lg-3 col-md-6">
+                                      <div class="panel panel-default">
+                                        <div class="panel-header">
+                                          <span class="col-xs-9 text-right">CHA</span>
+                                          <div class="clearfix"></div>
+                                        </div>
+                                        <div class="panel-heading">
+                                          <div class="row">
+
+                                            <div class="col-xs-9 text-right">
+                                              <div class="huge">12</div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <a href="#">
+                                          <div class="panel-footer">
+                                            <span class="col-xs-9 text-right">5</span>
+                                            <div class="clearfix"></div>
+                                          </div>
+                                        </a>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+
+                </div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+
+              </div>
+
+            </center>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
