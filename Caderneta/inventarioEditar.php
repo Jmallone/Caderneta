e50d0fa0ca5b9e8874cf2ee73e9559949cb5b8bc
Michel G. Souza <Michelgomesdes@hotmail.com>
2017-11-30 00:12:14 -0200

    Adicionando os CRUD 1 para n

--- Caderneta/inventarioEditar.php
+++ Caderneta/inventarioEditar.php
@@ -5,45 +5,59 @@
   <div class="container-fluid">
     <!-- Page Heading -->
     <form name="Cadastro" action="" method="GET">
-      <div id="minhaDiv>
+      <div id="minhaDiv"  style="">
         <table class="table table-hover" style="width: 63%;">
         </tr>
         <tr>
-          <td><input style="display: none;" name="site" value='classeseditar'></td>
-          <td><input style="display: none;" name="op" value='1'></td>
-          <td><input style="display: none;" name="id" value=<?php echo $_GET["id"]; ?>></td>
-          <td><input class="form-control" placeholder="Nome" name="nome" value="<?php echo $_GET["nome"]; ?>"</td>
-          <td><input class="form-control" placeholder="HitDice" name="hitdice" value="<?php echo $_GET["hitdice"]; ?>"></td>
-          <td class="actions">
 
-            <input type="submit"  class="btn btn-success x" value="OK" />
+          <?php
+          if(isset($_GET["op"]) && $_GET["op"] == 3){
+
+              echo "ADICIONADO";
+          }else{
+            echo "<td><input style='display: none;' name='site' value='inventarioEditar'></td>
+            <td><input style='' name='NomeInventario' value='".$_GET["id"]."'></td>
+            <td><input style='display: none;' name='NomeInventarioOLD' value='".$_GET["id"]."'></td>
+            <td><input style='display: none;' name='Ficha_idFicha' value='".$_GET["idFicha"]."'></td>
+            <td><input style='' name='descricaoInventario' value='".$_GET["desc"]."'></td>
+            <td><input style='' name='quantidadeInventario' value='".$_GET["qtd"]."'></td>
+            <td><input style='display: none;' name='op' value='3'></td>
+            ";
+
+          }
+
+          ?>
+          <td>
+            <div class="row">
+
+            </div>
+
+            <div class="row">
+
+              <input type="submit"  class="btn btn-success x" value="OK" />
+            </div>
             <?php
-            if(isset($_GET["op"]) && $_GET["op"] == 1){
+            if(isset($_GET["op"]) && $_GET["op"] == 3){
 
-              $sql = "UPDATE `classe` SET `nomeClasse` = '".$_GET["nome"]."', `dadoVida` = '".$_GET["hitdice"]."' WHERE `classe`.`idClasse` = ".$_GET["id"]."";
 
-              if ($conn->query($sql) === TRUE) {
-                echo "Record updated successfully";
+              $sql = "UPDATE `inventario` SET `NomeInventario`='" . $_GET["NomeInventario"] ."', `descricaoInventario`='" . $_GET["descricaoInventario"] ."', `quantidadeInventario`='" . $_GET["quantidadeInventario"] ."' WHERE `NomeInventario`='" . $_GET["NomeInventarioOLD"] ."' and`Ficha_idFicha`='" . $_GET["Ficha_idFicha"] ."'";
+              if (mysqli_query($conn, $sql)) {
+                echo "Adicionado com Sucesso";
               } else {
-                echo "Error updating record: " . $conn->error;
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
               }
 
-              $conn->close();
-            }
-            ?>
-          </td>
-        </tr>
+            } ?>
 
-      </tbody>
-    </table>
+
+          </table>
+        </div>
+      </form>
+
+
+    </div>
   </div>
-
-</form>
-
-
-</div>
-</div>
-<!-- /.row -->
+  <!-- /.row -->
 
 </div>
 <!-- /.container-fluid -->
